2074a009fa3224c55d35608fdbdb8e032c4cf508
may873 <vitalliquidanoemmanuel@cetis50cdmx.com>
2024-10-15 00:14:28 -0600

    se sube la versión 1.2 del Quizziz

--- Quizziz/Cuestionario/main.py
+++ Quizziz/Cuestionario/main.py
@@ -2,7 +2,24 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title=("Quizziz")
+    page.bgcolor="black"
+
+    txtR1=ft.TextField(label="ingresa el indice de tu respuesta")
+    
+    lblP1=ft.Text("¿Que es la tecnologia?",color="white")
+    lblia=ft.Text("a)",color="white")
+    lblib=ft.Text("b)",color="white")
+    lblic=ft.Text("c)",color="white")
+
+    page.add(
+        ft.Column(
+            controls=[
+                ft.Row(controls=[lblP1], alignment="CENTER"),
+                ft.Row(controls=[lblia,lblib,lblic],alignment="CENTER"),
+                ft.Row(controls=[txtR1],alignment="CENTER"),
+                ],alignment="CENTER")
+        )
 
 
 ft.app(main)
